66283f6c60cd303f0f685e858d2da4bd65409f70
matthmeyer <maetthy@arcor.de>
2022-12-03 16:37:14 +0100

    Add index page

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from search.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index, name="index"),
 ]
