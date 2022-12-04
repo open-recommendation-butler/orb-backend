7732013951694a827ac8afb0e4d2a64418bf6c8c
matthmeyer <maetthy@arcor.de>
2022-12-04 23:01:53 +0100

    Add suggest functionality

--- myproject/search/views.py
+++ myproject/search/views.py
@@ -19,10 +19,34 @@ def search(request):
   )
   response = query.execute()
   print(response.to_dict())
+
+  suggest = Article.search().suggest(name="my-suggest", text=queryString, term={'field': 'teaser'})
+  suggestResponse = suggest.execute()
+  print('suggestResponse', suggestResponse.to_dict())
+  suggestion = []
+  suggestion_html = []
+  print('___')
+  for token in suggestResponse.suggest['my-suggest']:
+    try:
+      suggestion.append(token['options'][0]['text'])
+      suggestion_html.append(f"<b><i>{token['options'][0]['text']}</i></b>")
+    except IndexError:
+      suggestion.append(token['text'])
+      suggestion_html.append(token['text'])
+
+  suggestion = " ".join(suggestion)
+  suggestion_html = " ".join(suggestion_html)
+  if suggestion == queryString:
+    suggestion = None
+    suggestion_html = None
+
+
   context = {
     "articles": query, 
     "queryString": queryString, 
-    "took": response.took / 1000, 
-    "hitCount": response.hits.total.value
+    "took": (response.took + suggestResponse.took) / 1000, 
+    "hitCount": response.hits.total.value,
+    "suggestion": suggestion,
+    "suggestion_html": suggestion_html
   }
   return render(request, 'search/results.html', context)
