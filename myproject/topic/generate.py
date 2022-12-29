9567ef46a101ae3a6a8ebdb36cebd2f51efd8fae
matthmeyer <maetthy@arcor.de>
2022-12-29 19:52:54 +0100

    Add recursive topic modeling

--- myproject/topic/generate.py
+++ myproject/topic/generate.py
@@ -7,47 +7,51 @@ from numpy.linalg import norm
 def cosine_sim(a, b):
   return np.dot(a, b)/(norm(a) * norm(b))
 
+already_clustered_articles = set()
+
+def find_topic_recursive(article, articles):
+  topic = [article]
+  already_clustered_articles.add(article.meta.id)
+  for other_article in articles:
+    if other_article.meta.id in already_clustered_articles:
+      continue
+    sim = cosine_sim(article.embedding, other_article.embedding)
+    if sim > 0.4:
+      topic.append(other_article)
+      topic.extend(find_topic_recursive(other_article, articles))
+  return topic
+
 def generate():
   print('start')
   query = Article.search()
-  query = query.query(
-    'bool', 
-    must=[
-      Q('term', created=dateparser.parse("28.12.2022", settings={'DATE_ORDER': 'DMY'})),
-      #Q('term', content_type=content_type)
-    ]
-  )
-  # response = query.scan()
+  query = query.query('match_all')
 
   articles = []
   # Convert article embeddings to numpy arrays
   for article in query.scan():
-    #print(article.meta.id)
+    print(article.meta.id)
     article.embedding = np.array(article.embedding)
     articles.append(article)
   
-  
-  checked_articles = set()
+
+  topics = []
   for article in articles:
-    if article.meta.id in checked_articles:
+    if article.meta.id in already_clustered_articles:
       continue
-    checked_articles.add(article.meta.id)
-    other_articles = []
-    for other_article in articles:
-      if other_article.meta.id in checked_articles:
-        continue
-      sim = cosine_sim(article.embedding, other_article.embedding)
-      if sim > 0.4:
-        other_articles.append(other_article)
-        checked_articles.add(other_article.meta.id)
-        print()
-        print()
-        print(article.title)
-        print(article.teaser)
-        print('####')
-        print('____________________')
-        print(other_article.title)
-        print(other_article.teaser)
-        print('Sim', sim)
+    topics.append(find_topic_recursive(article, articles))
+  
+  for i, topic in enumerate(topics):
+    print()
+    print('::::::::::::::::::::::::::::::::::::')
+    print(f"TOPIC {i}")
+    print('::::::::::::::::::::::::::::::::::::')
+    for ii, article in enumerate(topic):
+      print(f"ARTICLE {ii}")
+      print(article.title)
+      print('_')
+      print(article.teaser)
+      print('_')
+      print(article.url)
+      print(article.meta.id)
 
   return
