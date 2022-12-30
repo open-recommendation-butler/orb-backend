724a8228da29a83aac9bfeb54e57a794f9b14cc6
matthmeyer <maetthy@arcor.de>
2022-12-30 10:18:30 +0100

    Refactor topic generation

--- myproject/topic/generate.py
+++ myproject/topic/generate.py
@@ -1,42 +1,65 @@
 from article.documents import Article
-from elasticsearch_dsl import Q
-import dateparser
 import numpy as np
 from numpy.linalg import norm
 
 def cosine_sim(a, b):
   return np.dot(a, b)/(norm(a) * norm(b))
 
-already_clustered_articles = set()
-
-def find_topic_recursive(article, articles):
+def find_topic_recursive(article, articles, already_clustered_articles):
   topic = [article]
   already_clustered_articles.add(article.meta.id)
+
   for other_article in articles:
+
+    # Ignore already clustered article
     if other_article.meta.id in already_clustered_articles:
       continue
+
+    # Calculate similartiy
     sim = cosine_sim(article.embedding, other_article.embedding)
-    if sim > 0.4:
-      topic.extend(find_topic_recursive(other_article, articles))
+
+    # Add the other article to the topic if similarity is over threshold
+    if sim > 0.5:
+
+      # Find further similar articles
+      t, already_clustered_articles = find_topic_recursive(other_article, articles, already_clustered_articles)
+      topic.extend(t)
+
   return topic
 
-def generate():
+  
+def generate(articles):
+  topics = []
+
+  # Collect all already clustered articles in a set
+  already_clustered_articles = set()
+
+  # Convert embeddings to numpy arrays
+  for article in articles:
+    article.embedding = np.array(article.embedding)
+
+  # Iterate through articles and create recursive topics
+  for article in articles:
+
+    # Ignore already clustered articles
+    if article.meta.id in already_clustered_articles:
+      continue
+
+    topic, already_clustered_articles = find_topic_recursive(article, articles, already_clustered_articles)
+    topics.append(topic)
+
+  return topics
+
+def generate_for_all_articles():
   query = Article.search()
   query = query.query('match_all')
   response = query.execute()
 
   articles = []
-  # Convert article embeddings to numpy arrays
-  for article in query.scan():
+
+  for i, article in enumerate(query.scan()):
     article.embedding = np.array(article.embedding)
     articles.append(article)
   
-
-  topics = []
-  for article in articles:
-    if article.meta.id in already_clustered_articles:
-      continue
-    topics.append(find_topic_recursive(article, articles))
-
-
-  return
+  topics = generate(articles)
+  return topics
