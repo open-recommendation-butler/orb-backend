6174c762ddb43e409d51bd6b3325aa3268d03e3d
matthmeyer <maetthy@arcor.de>
2023-01-10 12:18:46 +0100

    Add prints

--- myproject/topic/run.py
+++ myproject/topic/run.py
@@ -1,4 +1,15 @@
-from topic.generate import generate, generate_for_all_articles
+from topic.generate import generate_for_all_articles
+import time
 
+start = time.time()
 topics = generate_for_all_articles()
 print(topics)
+end = time.time()
+
+for topic in topics:
+  if len(topic.articles) > 1:
+    print()
+    for article in topic.articles:
+      print(article.portal, '|', article.title)
+
+print(f'Time:', end - start)
