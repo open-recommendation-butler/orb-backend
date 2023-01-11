3183d6ef29667f45e3df43f5016bbebc39862320
matthmeyer <maetthy@arcor.de>
2023-01-11 12:33:05 +0100

    Add clear_index command

---
+++ myproject/article/management/commands/clear_index.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand, CommandError
+from article.documents import Article
+from elasticsearch_dsl import Index
+
+class Command(BaseCommand):
+  help = 'Wipes out your entire search index. Use with caution.'
+
+  def add_arguments(self, parser):
+    pass
+
+  def handle(self, *args, **options):
+    self.stdout.write(
+      "Removing all documents from your index because you said so."
+    )
+    indx = Index('article')
+    indx.delete()
+
+    Article.init()
+    self.stdout.write(
+      "Done."
+    )
