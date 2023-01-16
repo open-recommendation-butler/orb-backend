08316680bbb8a33dec9d624276bfb6ce4460d0d3
matthmeyer <maetthy@arcor.de>
2023-01-16 14:19:23 +0100

    Add DOMAIN to ALLOWED_HOSTS

--- app/app/settings.py
+++ app/app/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG", default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env("DOMAIN", default=""), '.localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
