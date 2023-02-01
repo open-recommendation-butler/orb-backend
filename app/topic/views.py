f238922dbb5a5fa6544e5c528d4ee128f52225ca
matthmeyer <maetthy@arcor.de>
2023-02-01 03:19:41 +0100

    Create topic endpoint

--- app/topic/views.py
+++ app/topic/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .documents import Topic
+from elasticsearch import NotFoundError
+from django.http import Http404
+from topic.generate import generate_for_all_articles
+class TopicView(APIView):
+  def get_object(self, pk):
+    try:
+      a = Topic.get(pk)
+      return a
+    except NotFoundError:
+      raise Http404
 
-# Create your views here.
+  def post(self, request, format=None):
+    topics = generate_for_all_articles()
+
+    for topic in topics:
+      if len(topic.articles) > 1:
+        print()
+        for article in topic.articles:
+          print(article.portal, '|', article.title)
+    
+    return Response(status=status.HTTP_201_CREATED)
