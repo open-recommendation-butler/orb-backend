4f685d4ff05011d9436970a588ab1115a66fc541
matthmeyer <maetthy@arcor.de>
2023-02-07 21:28:00 +0100

    Add keywords to topics

--- app/topic/serializers.py
+++ app/topic/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from article.serializers import ArticleSerializer
 
+class KeywordsField(serializers.ListField):
+    child = serializers.CharField()
+
 class TopicSerializer(serializers.Serializer):
   title=serializers.CharField()
   article_count=serializers.IntegerField()
+  category=serializers.CharField(required=False)
   created=serializers.DateTimeField()
+  keywords=KeywordsField(required=False)
   articles=ArticleSerializer(many=True)
