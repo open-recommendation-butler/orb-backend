b22f995f6348746a48e36b5534ba5c637fccec77
matthmeyer <maetthy@arcor.de>
2023-02-13 21:44:57 +0100

    Add keyword and article_count field

--- app/topic/models.py
+++ app/topic/models.py
@@ -1,5 +1,17 @@
 from django.db import models
-
+from collections import Counter
 class Topic:
   def __init__(self):
     self.articles = []
+    self.article_count = None
+    self.keywords = []
+  
+  def get_keywords(self):
+    all_occurences = []
+    for article in self.articles:
+      all_occurences.extend(article.keywords)
+    counted = Counter(all_occurences)
+    self.keywords = [x[0] for x in counted.most_common()]
+  
+  def get_article_count(self):
+    self.article_count = len(self.articles)
