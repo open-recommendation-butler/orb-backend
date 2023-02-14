05c211410883a4d76453faeaf24bbe445c1ed4f2
matthmeyer <maetthy@arcor.de>
2023-02-14 16:19:51 +0100

    Adjust settings for React web gui

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = [env("DOMAIN", default=""), '.localhost', '127.0.0.1', '[::1]']
 
 CSRF_TRUSTED_ORIGINS = [f'http://{env("DOMAIN", default="")}', f'https://{env("DOMAIN", default="")}', 'http://localhost', 'http://127.0.0.1']
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'https://open-recommendation-butler.tech']
 
 # Application definition
 
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/api/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
