441eab70940ba7396ca8b7853438b8f56c3834d9
matthmeyer <maetthy@arcor.de>
2023-02-14 15:02:09 +0100

    Add pagination

--- web-gui/src/pages/searchPage.js
+++ web-gui/src/pages/searchPage.js
@@ -9,6 +9,12 @@ function SearchPage() {
   let [searchParams, setSearchParams] = useSearchParams();
   const [query, setQuery] = useState("");
   const [results, setResults] = useState();
+  const [page, setPage] = useState();
+  const [pageList, setPageList] = useState([]);
+
+  const HITS_PER_PAGE = 20;
+  let page_list;
+  
 
   let search = (query) => {
     GET(`/search/?q=${query}&count=20`)
@@ -20,7 +26,42 @@ function SearchPage() {
       setQuery(q);
       search(q);
     };
+    let p = searchParams.get('page');
+    if (p) {
+      setPage(parseInt(p));
+    } else {
+      setPage(1);
+    }
   }, []);
+
+  useEffect(() => {
+    if (!results) return;
+    let start = 1;
+    let end = 5;
+    if (page < (Math.ceil(results.hitCount / HITS_PER_PAGE) - 2)) {
+      if (page < 3) {
+        console.log('p0')
+        if (Math.ceil(results.hitCount / HITS_PER_PAGE) < 5) {
+          console.log('p1')
+          end = Math.ceil(results.hitCount / HITS_PER_PAGE);
+        }
+      } else {
+        console.log('p2')
+        end = page + 2;
+        start = page - 2;
+      }
+    } else {
+      end = Math.ceil(results.hitCount / HITS_PER_PAGE);
+      if (page < 5) {
+        console.log('p3')
+        start = 1;
+      } else {
+        console.log('p4')
+        start = end - 5;
+      }
+    }
+    setPageList(Array.from({length: end - start + 1}, (x, i) => i + start))
+  }, [results]);
 
   return (
       <div className="container mx-auto pt-16 px-3 mb-40">
@@ -45,6 +86,25 @@ function SearchPage() {
                     : <ArticleListElement article={content} />
                 )})
               }
+              <div className='mt-14'>
+                <a 
+                  href={`/search?q=${query}&page=${page - 1}`}
+                  disabled={page == 1 ? true : false} 
+                  className={`rounded-l-full border px-3 py-2 font-bold ${page == 1 ? "bg-slate-200 text-gray-500 cursor-not-allowed" : "hover:bg-slate-100 cursor-pointer"}`}
+                >Zurück</a>
+                {pageList.map((site_page) => 
+                  <a 
+                    href={`/search?q=${query}&page=${site_page}`}
+                    disabled={page == site_page ? true : false} 
+                    className={`border-y border-r px-3 py-2 font-bold ${page == site_page ? "bg-slate-200 text-gray-500 cursor-not-allowed" : "hover:bg-slate-100 cursor-pointer"}`}
+                  >{site_page}</a>
+                )}
+                <a 
+                  href={`/search?q=${query}&page=${page + 1}`}
+                  disabled={page == Math.ceil(results.hitCount / HITS_PER_PAGE) ? true : false} 
+                  className={`rounded-r-full border-y border-r px-3 py-2 font-bold ${page == Math.ceil(results.hitCount / HITS_PER_PAGE) ? "bg-slate-200 text-gray-500 cursor-not-allowed" : "hover:bg-slate-100 cursor-pointer"}`}
+                >Weiter</a>
+              </div>
             </div>
           }
         </div>
