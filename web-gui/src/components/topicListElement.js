292127fd89652dce72d3f8579f02bb6f05c30314
matthmeyer <maetthy@arcor.de>
2023-02-14 13:11:18 +0100

    Add seperator line around topics

--- web-gui/src/components/topicListElement.js
+++ web-gui/src/components/topicListElement.js
@@ -3,12 +3,15 @@ import ArticleListElement from './articleListElement';
 
 function TopicListElement({ topic }) {
   return (
-    <div className='mb-4'>
-      <p className='text-m font-bold'>{ topic.keywords.slice(0, 3).join(", ") }</p>
-      <div className='grid md:grid-cols-2 gap-x-3'>
-        {topic.articles.map(article => <ArticleListElement article={article} />)}
-      </div>
-    </div>
+    topic.article_count === 1
+      ? <ArticleListElement article={topic.articles[0]} />
+      :
+        <div className='mb-4 border-t border-b pt-2'>
+          <p className='text-m font-bold'>{ topic.keywords.slice(0, 3).join(", ") }</p>
+          <div className='grid md:grid-cols-2 gap-x-3'>
+            {topic.articles.map(article => <ArticleListElement article={article} />)}
+          </div>
+        </div>
   )
 }
 
