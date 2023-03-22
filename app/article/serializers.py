0a816767d33470b08ad288c964c7c1a20018479f
matthmeyer <maetthy@arcor.de>
2023-03-22 22:24:24 +0100

    Add highlighting

--- app/article/serializers.py
+++ app/article/serializers.py
@@ -11,13 +11,51 @@ class ArticleSerializer(serializers.Serializer):
   keywords = serializers.ListField(required=False)
   url = serializers.URLField(required=False)
   created = serializers.DateTimeField(required=False)
-  highlight = serializers.SerializerMethodField()
+  highlights = serializers.SerializerMethodField()
 
-  def get_highlight(self, obj):
+  def get_highlights(self, obj):
+    highlights = []
     try:
+      for highlight in obj.meta.highlight.fulltext:
+        timestamp = None
+        timestamp_in_seconds = None
+        for line in highlight.split('\n')[1:]:
+          if '-->' in line:
+            timestamp = line.split('.')[0]
+            timestamp_in_seconds = int((float(timestamp.split(':')[-1])))
+            try:
+              timestamp_in_seconds += int(timestamp.split(':')[-2]) * 60
+            except ValueError:
+              pass
+            try:
+              timestamp_in_seconds += int(timestamp.split(':')[-3]) * 60 * 60
+            except IndexError:
+              pass
+            break
+
+        highlight = " ".join([x for x in highlight.split('\n') if '-->' not in x])
+        highlight = highlight.strip()
+        if highlight.endswith('.'):
+          highlight += '..'
+        else:
+          highlight += '...'
+        
+        highlight = '...' + highlight
+        if timestamp:
+          highlights.append({
+            "timestamp": timestamp,
+            "timestamp_in_seconds": timestamp_in_seconds,
+            "highlight": highlight
+          })
+        else:
+          highlights.append({"highlight": highlight})
+    except AttributeError:
+      pass
+
+    if not highlights:
       try:
-        return obj.meta.highlight.teaser[0]
-      except:
-        return obj.meta.highlight.fulltext[0]
-    except:
-      return obj.teaser
+        highlights.append({"highlight": obj.meta.highlight.teaser[0]})
+      except AttributeError:
+        highlights.append({"highlight": obj.teaser})
+    
+    return highlights
