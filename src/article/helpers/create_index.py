ac81069ee8e0ad902603d7dd811140347650857b
matthmeyer <maetthy@arcor.de>
2023-04-11 12:24:06 +0200

    Remove logger

--- src/article/helpers/create_index.py
+++ src/article/helpers/create_index.py
@@ -1,11 +1,9 @@
 from article.documents import Article
 from topic.documents import Topic
 from suggestion.documents import Suggestion
-from logger.documents import Logger
 from elasticsearch_dsl import Index
 
 Article.init()
 Topic.init()
 Suggestion.init()
-Logger.init()
 print('Index created.')
